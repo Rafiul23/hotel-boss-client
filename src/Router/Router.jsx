2a699ec6f6f404ee2cd8558e81d98ab61e799fa2
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-17 19:58:13 +0600

    added other components

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from './../Root/Root';
 import Home from "../Components/Home/Home";
+import Rooms from '../Components/Rooms/Rooms';
+import Register from './../Components/Register/Register';
+import Login from './../Components/Login/Login';
+import Contact from './../Components/Contact/Contact';
+import Dashboard from './../Components/Dashboard/Dashboard';
+import About from './../Components/About/About';
 
 const router = createBrowserRouter([
     {
@@ -10,6 +16,30 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+          path: '/rooms',
+          element: <Rooms></Rooms>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/contact',
+          element: <Contact></Contact>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        },
+        {
+          path: '/dashboard',
+          element: <Dashboard></Dashboard>
         }
       ]
     },
