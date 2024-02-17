f1f521c9603c243f196530db0e7aeec62d2e5daa
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-17 12:16:36 +0600

    added header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="py-4 bg-rose-600 text-center">
+            <h1 className=" text-white text-2xl font-bold">Hotel Nikunja</h1>
+        </div>
+    );
+};
+
+export default Header;
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Header from "../Components/Header/Header";
 
 
 const Root = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto">
+            <Header></Header>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
