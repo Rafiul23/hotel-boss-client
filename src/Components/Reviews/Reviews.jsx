a19ac1efc4c938a1a8fd545229c9f24f46f8797d
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-24 12:18:00 +0600

    fetched reviews and set to an array

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 
 
 const Reviews = () => {
 
-    
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(()=>{
+        fetch('reviews.json')
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    },[])
 
     return (
         <div className="my-5">
