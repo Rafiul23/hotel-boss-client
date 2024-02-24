af6649ec1355451701185534a0f5f10cfe40460f
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-24 13:22:29 +0600

    added footer in home page

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
 
 
 const Root = () => {
@@ -9,6 +10,7 @@ const Root = () => {
             <Header></Header>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
