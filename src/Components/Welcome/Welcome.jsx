7075065b4a2c3afc5ed8c8388dcf943393c2a3a9
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-24 10:28:42 +0600

    added welcome message

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -2,8 +2,9 @@
 
 const Welcome = () => {
     return (
-        <div>
-            
+        <div className="my-5">
+            <h2 className="text-2xl font-bold text-center text-[#6a3b37]">Welcome to Hotel Nikunja</h2>
+            <p className="text-center">We are here to provide you a decent service at reasonable price</p>
         </div>
     );
 };
