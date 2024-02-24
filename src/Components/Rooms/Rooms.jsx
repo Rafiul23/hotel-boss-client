f3d443c19a3b160aa9e506b8120c3390d99c1ca4
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-24 17:03:13 +0600

    added fake data for rooms

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -5,6 +5,7 @@ const Rooms = () => {
         <div>
             <h2 className="text-4xl my-4 font-bold text-center text-[#6a3b37]">All Rooms Are Here</h2>
             <p className="text-center text-2xl">Choose your favorite room from here and book your available slot.</p>
+            
         </div>
     );
 };
