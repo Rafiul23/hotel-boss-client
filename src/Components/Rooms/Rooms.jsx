55db53b4a30b2baabb3eb35a1b10a2d395ec8d25
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-24 18:53:27 +0600

    added image in json data

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 
 
 const Rooms = () => {
+
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        fetch('rooms.json')
+            .then(res => res.json())
+            .then(data => setRooms(data))
+    }, [])
+
     return (
         <div>
             <h2 className="text-4xl my-4 font-bold text-center text-[#6a3b37]">All Rooms Are Here</h2>
             <p className="text-center text-2xl">Choose your favorite room from here and book your available slot.</p>
-            
+            <div>
+
+            </div>
         </div>
     );
 };
